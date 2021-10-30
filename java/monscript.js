443bb45c68d08b95cc781b0bbc08559b52a28a86
Fly-lyric-and-you <77666846+Fly-lyric-and-you@users.noreply.github.com>
2021-10-30 14:39:45 +0200

    Add files via upload

--- java/monscript.js
+++ java/monscript.js
@@ -9,131 +9,70 @@ function closeNavig() {
   document.getElementById("myNavig").style.height = "0%";
 }
 
-/*---------------------------------------------------*/
-
-function openNav() {
-document.getElementById("myNav").style.width = "100%";
-}
-
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
-/*---------------------------------------------------*/
-
-function openNav1() {
-document.getElementById("myNav1").style.width = "100%";
-}
-
-
-function closeNav1() {
-  document.getElementById("myNav1").style.width = "0%";
-}
-
-/*---------------------------------------------------*/
-
-function openNav2() {
-document.getElementById("myNav2").style.width = "100%";
-}
-
-
-function closeNav2() {
-  document.getElementById("myNav2").style.width = "0%";
-}
-/*---------------------------------------------------*/
-
-function openNav3() {
-document.getElementById("myNav3").style.width = "100%";
-}
-
-
-function closeNav3() {
-  document.getElementById("myNav3").style.width = "0%";
-}
-/*---------------------------------------------------*/
-
-function openNav4() {
-document.getElementById("myNav4").style.width = "100%";
-}
-
-
-function closeNav4() {
-  document.getElementById("myNav4").style.width = "0%";
-}
-/*---------------------------------------------------*/
-
-
-
-//____________________________________________________//
-
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
-  }
-}
-
-//____________________________________________________//
-
-
- function requestFullScreen() {
-
-  var el = document.body;
-
-  // Supports most browsers and their versions.
-  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
-  || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-  if (requestMethod) {
-
-    // Native full screen.
-    requestMethod.call(el);
-
-  } else if (typeof window.ActiveXObject !== "undefined") {
-
-    // Older IE.
-    var wscript = new ActiveXObject("WScript.Shell");
-
-    if (wscript !== null) {
-      wscript.SendKeys("{F11}");
-    }
-  }
-}
-
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function closeFunction() {
+  document.getElementById("myDropdown").classList.remove('show');
+}
+
+
+function init() {
+
+  const cityMap = {
+
+      lat: 44.837789,
+      long: -0.57918,
+  }
+
+  const zoomLevel = 12;
+
+
+  var mymap = L.map('mapid').setView([cityMap.lat, cityMap.long], zoomLevel);
+
+
+  var marker = L.marker([44.837789, -0.57918]).addTo(mymap);
+
+
+  var circle = L.circle([44.837789, -0.55], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+  }).addTo(mymap);
+  circle.bindPopup("warning!");
+
+
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicGF1bGFjZXR5bCIsImEiOiJja3Y4bHBpdjYycHRnMzFscGozOXpncHFnIn0.iY-4A4Yo1Riz09aPbzwk4w'
+  }).addTo(mymap);
+
+  if ("geolocation" in navigator) {
+    /* la géolocalisation est disponible */
+  } else {
+    /* la géolocalisation n'est pas disponible */
+  }
+
+
+  function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
 
+
+}
 
 
 
