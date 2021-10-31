c34260bc3afb9e731cf9bd3f8c647bb2d97ddda5
Fly-lyric-and-you <77666846+Fly-lyric-and-you@users.noreply.github.com>
2021-10-31 10:39:06 +0100

    Update monscript.js

--- java/monscript.js
+++ java/monscript.js
@@ -23,8 +23,11 @@ function init() {
 
   const cityMap = {
 
-      lat: 44.837789,
-      long: -0.57918,
+      //lat: 44.837789,
+      //long: -0.57918,
+
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
   }
 
   const zoomLevel = 12;
@@ -57,24 +60,11 @@ function init() {
 
   if ("geolocation" in navigator) {
     /* la géolocalisation est disponible */
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(init);
   } else {
     /* la géolocalisation n'est pas disponible */
   }
-
-var x = document.getElementById("demo");
-
-  function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
 
 
 
