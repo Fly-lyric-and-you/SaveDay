1ac080fbf5e00d4fe94f54fb2a608fe01a4badde
Fly-lyric-and-you <77666846+Fly-lyric-and-you@users.noreply.github.com>
2021-11-03 17:20:22 +0100

    Add files via upload

--- java/monscript.js
+++ java/monscript.js
@@ -19,7 +19,7 @@ function closeFunction() {
 }
 
 
-var x = document.getElementById("demo");
+/*var x = document.getElementById("demo");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -94,52 +94,60 @@ function init() {
 
   }
 
+}*/
+
+
+
+function init() {
+        navigator.geolocation.getCurrentPosition(lancerMap,showError);
 }
 
 
 
-/*function init() {
+function lancerMap(position) {
+  var mymap = L.map('mapid').setView([44.837789, -0.57918], 12);
 
 
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-
-      const zoomLevel = 12;
-
-      var mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude], zoomLevel);
+  var marker = L.marker([44.837789, -0.57918]).addTo(mymap);
 
 
-      var marker = L.marker([44.837789, -0.57918]).addTo(mymap);
-
-
-      var circle = L.circle([44.837789, -0.55], {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.5,
-          radius: 500
-      }).addTo(mymap);
-      circle.bindPopup("warning!");
+  var circle = L.circle([44.837789, -0.55], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+  }).addTo(mymap);
+  circle.bindPopup("warning!");
 
 
 
-      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: 'pk.eyJ1IjoicGF1bGFjZXR5bCIsImEiOiJja3Y4bHBpdjYycHRnMzFscGozOXpncHFnIn0.iY-4A4Yo1Riz09aPbzwk4w'
-      }).addTo(mymap);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicGF1bGFjZXR5bCIsImEiOiJja3Y4bHBpdjYycHRnMzFscGozOXpncHFnIn0.iY-4A4Yo1Riz09aPbzwk4w'
+  }).addTo(mymap);
+  
+}
 
-    });
-  } 
-
-  else 
-  {
-
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      document.getElementById("demo").innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      document.getElementById("demo").innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      document.getElementById("demo").innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      document.getElementById("demo").innerHTML = "An unknown error occurred."
+      break;
   }
-
-}*/
+}
 
 
 
